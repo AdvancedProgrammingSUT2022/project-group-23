7d04fd84caae7a3a9b6ee20f01f63b92e2f469b0
omid-d <hopebraves@gmail.com>
2022-04-17 22:26:30 +0430

    user model created

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,4 +1,84 @@
 package model;
 
+import java.util.ArrayList;
+
 public class User {
+    private String username;
+    private String password;
+    private String nickname;
+    private int score;
+    private int cup;
+    private int gold;
+    private int happiness;
+    private static ArrayList<User> users;
+    private ArrayList<Unit> units;
+    private ArrayList<City> cities;
+    private ArrayList<Technology> technologies;
+
+    public User(String username,String password,String nickname)
+    {
+        this.username=username;
+        this.password=password;
+        this.nickname=nickname;
+    }
+
+    public String getUsername () {
+        return username;
+    }
+
+    public String getPassword () {
+        return password;
+    }
+
+    public String getNickname () {
+        return nickname;
+    }
+
+    public int getScore () {
+        return score;
+    }
+
+    public int getCup () {
+        return cup;
+    }
+
+    public int getGold () {
+        return gold;
+    }
+
+    public int getHappiness () {
+        return happiness;
+    }
+
+    public static ArrayList<User> getUsers () {
+        return users;
+    }
+
+    public ArrayList<Unit> getUnits () {
+        return units;
+    }
+
+    public ArrayList<City> getCities () {
+        return cities;
+    }
+
+    public void addCity(City city)
+    {
+        cities.add(city);
+    }
+
+    public void addUnit(Unit unit)
+    {
+        units.add(unit);
+    }
+
+    public ArrayList<Technology> getTechnologies () {
+        return technologies;
+    }
+
+    public void addTechnology(Technology technology)
+    {
+        technologies.add(technology);
+    }
+
 }

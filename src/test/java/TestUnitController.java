00466ef2c0fe6b78bec5fb6fa496f1452ba1be53
omid-d <hopebraves@gmail.com>
2022-05-02 13:13:39 +0430

    test eliminate feature added

--- src/test/java/TestUnitController.java
+++ src/test/java/TestUnitController.java
@@ -3,10 +3,8 @@ import controller.CivilizationController;
 import controller.GameController;
 import controller.UnitController;
 import database.TechnologyDatabase;
-import model.Tile;
-import model.Unit;
-import model.User;
-import model.WorkerUnit;
+import database.TerrainDatabase;
+import model.*;
 import org.junit.After;
 import org.junit.Test;
 import org.mockito.Mock;
@@ -40,6 +38,28 @@ public class TestUnitController extends GameController {
         unitController.isTurnPossible();
         unitController.isTurnPossible();
         assertTrue(tiles[9][9].isRoad());
+    }
+
+    @Test
+    public void testEliminateFeature(){
+        players=new ArrayList<>();
+        User user=new User("omid","123","omid123");
+        players.add(user);
+        WorkerUnit unit=new WorkerUnit(9,9);
+        CivilizationController civilizationController=new CivilizationController(players);
+        CityController cityController=new CityController();
+        UnitController unitController=new UnitController(cityController);
+        for (Unit currentPlayerUnit : currentPlayer.getUnits()) {
+            currentPlayerUnit.setState("no");
+        }
+        GameController.setSelectedUnit(unit);
+        Feature feature=new Feature("Jungle", 0, 1 ,-1, 2, 25,-10);
+        tiles[9][9].setFeature(feature);
+        unitController.eliminateFeature();
+        for(int i=0;i<7;i++){
+            unitController.isTurnPossible();
+        }
+        assertEquals(tiles[9][9].getFeature(),null);
     }
 
     @After

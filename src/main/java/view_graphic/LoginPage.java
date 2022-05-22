d29799b1e36627089b00d80c7e6215a77a4cae99
reza <r4heidari@gmail.com>
2022-05-23 00:59:49 +0430

    minor changes to LoginPage and exit button added

--- src/main/java/view_graphic/LoginPage.java
+++ src/main/java/view_graphic/LoginPage.java
@@ -39,73 +39,61 @@ public class LoginPage {
     Label nicknameLabel;
     @FXML
     Label passwordLabel;
+
     public void initialize() {
         Platform.runLater(() -> borderPane.requestFocus());
-        usernameLabel.setTextFill(Color.rgb(232,200,22));
-        nicknameLabel.setTextFill(Color.rgb(232,200,22));
-        passwordLabel.setTextFill(Color.rgb(232,200,22));
+        usernameLabel.setTextFill(Color.rgb(232, 200, 22));
+        nicknameLabel.setTextFill(Color.rgb(232, 200, 22));
+        passwordLabel.setTextFill(Color.rgb(232, 200, 22));
         title.setFill(Color.rgb(1, 231, 212));
-        registerController=RegisterController.getInstance();
-        BackgroundSize backgroundSize=new BackgroundSize(BackgroundSize.AUTO,BackgroundSize.AUTO,false,true,true,true);
-        BackgroundImage backgroundImage=new BackgroundImage(new Image(getClass().getResource("/images/backgrounds/loginBackground.png").toExternalForm()),
+        registerController = RegisterController.getInstance();
+        BackgroundSize backgroundSize = new BackgroundSize(BackgroundSize.AUTO, BackgroundSize.AUTO, false, true, true, true);
+        BackgroundImage backgroundImage = new BackgroundImage(new Image(getClass().getResource("/images/backgrounds/loginBackground.png").toExternalForm()),
                 BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,
                 backgroundSize);
         borderPane.setBackground(new Background(backgroundImage));
+
+        registerButton.setDisable(true);
+        loginButton.setDisable(true);
+        text = new Text();
+        vbox.getChildren().add(text);
     }
 
     public void type(KeyEvent keyEvent) {
         int strength = password.getText().length();
-        if (strength < 5){
+        if (strength < 5) {
             password.setStyle("-fx-border-color: #ff0066;");
             registerButton.setDisable(true);
             loginButton.setDisable(true);
-        }else{
+        } else {
             password.setStyle("-fx-border-width: 0");
             registerButton.setDisable(false);
             loginButton.setDisable(false);
         }
     }
 
-    public void register(){
-        String output=registerController.addUser(username.getText(),nickname.getText(),password.getText());
-        if(output.equals("user created successfully!")){
-            if(text==null) {
-                text = new Text();
-                text.setText(output);
-                text.setFill(Color.GREEN);
-                vbox.getChildren().add(text);
-            }else {
-                text.setFill(Color.GREEN);
-                text.setText(output);
-            }
-        }else {
-            if(text==null) {
-                text = new Text();
-                text.setText(output);
-                text.setFill(Color.RED);
-                vbox.getChildren().add(text);
-            }else {
-                text.setFill(Color.RED);
-                text.setText(output);
-            }
+    public void register() {
+        String output = registerController.addUser(username.getText(), nickname.getText(), password.getText());
+        if (output.equals("user created successfully!")) {
+            text.setFill(Color.GREEN);
+            text.setText(output);
+        } else {
+            text.setFill(Color.RED);
+            text.setText(output);
         }
     }
 
-    public void login (MouseEvent mouseEvent) throws Exception {
-        String output= registerController.login(username.getText(),password.getText());
-        if(output.equals("user logged in successfully!")){
-
-        }else {
-            if(text==null) {
-                text = new Text();
-                text.setText(output);
-                text.setFill(Color.RED);
-                vbox.getChildren().add(text);
-            }else {
+    public void login(MouseEvent mouseEvent) throws Exception {
+        String output = registerController.login(username.getText(), password.getText());
+        if (output.equals("user logged in successfully!")) {
+            //TODO enter main menu
+        } else {
                 text.setFill(Color.RED);
                 text.setText(output);
-            }
         }
     }
 
+    public void exit(MouseEvent mouseEvent) {
+        System.exit(0);
+    }
 }
